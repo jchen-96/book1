30ed0ab72bc01a0ce666e9de08de2498e4531f24
jchenCode <jchenCode@163.com>
2018-07-18 16:18:14 +0800

    daily commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int a, b;
-    while (cin >> a >> b) {
+    while (cin >> a >> b)
+    {
         cout << a + b << endl;
     }
     return 0;
